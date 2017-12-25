8dcab7b277b23848e0020c7de7986d5f2fdc7d22
Steve Mokris <smokris@softpixel.com>
2017-12-25 15:29:01 -0500

    Update minimum macOS version.  [#13885]

--- conanfile.py
+++ conanfile.py
@@ -3,16 +3,20 @@ import os
 
 class LibcsvConan(ConanFile):
     name = 'libcsv'
-    version = '3.0.3'
+
+    source_version = '3.0.3'
+    package_version = '2'
+    version = '%s-%s' % (source_version, package_version)
+
     settings = 'os', 'compiler', 'build_type', 'arch'
     url = 'https://github.com/vuo/conan-libcsv'
     license = 'https://sourceforge.net/projects/libcsv/'
     description = 'A library for reading and writing comma-separated data files'
-    source_dir = 'libcsv-%s' % version
+    source_dir = 'libcsv-%s' % source_version
     build_dir = '_build'
 
     def source(self):
-        tools.get('https://sourceforge.net/projects/libcsv/files/libcsv/libcsv-%s/libcsv-%s.tar.gz' % (self.version, self.version),
+        tools.get('https://sourceforge.net/projects/libcsv/files/libcsv/libcsv-%s/libcsv-%s.tar.gz' % (self.source_version, self.source_version),
                   sha256='d9c0431cb803ceb9896ce74f683e6e5a0954e96ae1d9e4028d6e0f967bebd7e4')
 
     def build(self):
@@ -20,7 +24,7 @@ class LibcsvConan(ConanFile):
         with tools.chdir(self.build_dir):
             autotools = AutoToolsBuildEnvironment(self)
             autotools.cxx_flags.append('-Oz')
-            autotools.cxx_flags.append('-mmacosx-version-min=10.8')
+            autotools.cxx_flags.append('-mmacosx-version-min=10.10')
             autotools.link_flags.append('-Wl,-install_name,@rpath/libcsv.dylib')
             autotools.configure(configure_dir='../%s' % self.source_dir,
                                 args=['--quiet',
