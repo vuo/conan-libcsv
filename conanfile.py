7a60c951c1eb0750181f4467214032db801d620b
Steve Mokris <steve@kosada.com>
2020-11-05 21:07:12 -0500

    Build for both x86_64 + arm64.  [kosada/vuo/vuo#17836]

--- conanfile.py
+++ conanfile.py
@@ -6,16 +6,24 @@ class LibcsvConan(ConanFile):
     name = 'libcsv'
 
     source_version = '3.0.3'
-    package_version = '4'
+    package_version = '5'
     version = '%s-%s' % (source_version, package_version)
 
-    build_requires = 'llvm/3.3-5@vuo/stable'
+    build_requires = (
+        'llvm/5.0.2-1@vuo/stable',
+        'macos-sdk/11.0-0@vuo/stable',
+    )
     settings = 'os', 'compiler', 'build_type', 'arch'
-    url = 'https://github.com/vuo/conan-libcsv'
-    license = 'https://sourceforge.net/projects/libcsv/'
+    url = 'https://github.com/rgamble/libcsv'
+    license = 'https://github.com/rgamble/libcsv/blob/master/LICENSE'
     description = 'A library for reading and writing comma-separated data files'
     source_dir = 'libcsv-%s' % source_version
-    build_dir = '_build'
+
+    build_x86_dir = '_build_x86'
+    build_arm_dir = '_build_arm'
+    install_x86_dir = '_install_x86'
+    install_arm_dir = '_install_arm'
+    install_universal_dir = '_install_universal_dir'
 
     def requirements(self):
         if platform.system() == 'Linux':
@@ -30,37 +38,64 @@ class LibcsvConan(ConanFile):
         self.run('mv %s/COPYING.LESSER %s/%s.txt' % (self.source_dir, self.source_dir, self.name))
 
     def build(self):
-        tools.mkdir(self.build_dir)
-        with tools.chdir(self.build_dir):
-            autotools = AutoToolsBuildEnvironment(self)
+        autotools = AutoToolsBuildEnvironment(self)
 
-            # The LLVM/Clang libs get automatically added by the `requires` line,
-            # but this package doesn't need to link with them.
-            autotools.libs = ['c++abi']
+        # The LLVM/Clang libs get automatically added by the `requires` line,
+        # but this package doesn't need to link with them.
+        autotools.libs = []
 
-            autotools.flags.append('-Oz')
-            autotools.flags.append('-Wno-error')
+        autotools.flags.append('-Oz')
+        autotools.flags.append('-Wno-error')
 
-            if platform.system() == 'Darwin':
-                autotools.flags.append('-mmacosx-version-min=10.10')
-                autotools.link_flags.append('-Wl,-headerpad_max_install_names')
-                autotools.link_flags.append('-Wl,-install_name,@rpath/libcsv.dylib')
+        if platform.system() == 'Darwin':
+            autotools.flags.append('-isysroot %s' % self.deps_cpp_info['macos-sdk'].rootpath)
+            autotools.flags.append('-mmacosx-version-min=10.11')
+            autotools.link_flags.append('-Wl,-headerpad_max_install_names')
+            autotools.link_flags.append('-Wl,-install_name,@rpath/libcsv.dylib')
 
-            env_vars = {
-                'CC' : self.deps_cpp_info['llvm'].rootpath + '/bin/clang',
-                'CXX': self.deps_cpp_info['llvm'].rootpath + '/bin/clang++',
-            }
-            with tools.environment_append(env_vars):
+        common_configure_args = [
+            '--quiet',
+            '--enable-shared',
+            '--disable-static',
+        ]
+
+        env_vars = {
+            'CC' : self.deps_cpp_info['llvm'].rootpath + '/bin/clang',
+            'CXX': self.deps_cpp_info['llvm'].rootpath + '/bin/clang++',
+        }
+        with tools.environment_append(env_vars):
+            build_root = os.getcwd()
+
+            self.output.info("=== Build for x86_64 ===")
+            tools.mkdir(self.build_x86_dir)
+            with tools.chdir(self.build_x86_dir):
+                autotools.flags.append('-arch x86_64')
+                autotools.link_flags.append('-arch x86_64')
                 autotools.configure(configure_dir='../%s' % self.source_dir,
-                                    args=['--quiet',
-                                          '--enable-shared',
-                                          '--disable-static',
-                                          '--prefix=%s' % os.getcwd()])
-                autotools.make(args=['install'])
+                                    build=False,
+                                    host=False,
+                                    args=common_configure_args + [
+                                        '--prefix=%s/%s' % (build_root, self.install_x86_dir),
+                                    ])
+                autotools.make(args=['--quiet'])
+                autotools.make(target='install', args=['--quiet'])
 
-            if platform.system() == 'Linux':
-                patchelf = self.deps_cpp_info['patchelf'].rootpath + '/bin/patchelf'
-                self.run('%s --set-soname libcsv.so lib/libcsv.so' % patchelf)
+            self.output.info("=== Build for arm64 ===")
+            tools.mkdir(self.build_arm_dir)
+            with tools.chdir(self.build_arm_dir):
+                autotools.flags.remove('-arch x86_64')
+                autotools.flags.append('-arch arm64')
+                autotools.link_flags.remove('-arch x86_64')
+                autotools.link_flags.append('-arch arm64')
+                autotools.configure(configure_dir='../%s' % self.source_dir,
+                                    build=False,
+                                    host=False,
+                                    args=common_configure_args + [
+                                        '--prefix=%s/%s' % (build_root, self.install_arm_dir),
+                                        '--host=x86_64-apple-darwin15.0.0',
+                                    ])
+                autotools.make(args=['--quiet'])
+                autotools.make(target='install', args=['--quiet'])
 
     def package(self):
         if platform.system() == 'Darwin':
@@ -68,8 +103,12 @@ class LibcsvConan(ConanFile):
         elif platform.system() == 'Linux':
             libext = 'so'
 
-        self.copy('*.h', src='%s/include' % self.build_dir, dst='include')
-        self.copy('libcsv.%s' % libext, src='%s/lib' % self.build_dir, dst='lib')
+        tools.mkdir(self.install_universal_dir)
+        with tools.chdir(self.install_universal_dir):
+            self.run('lipo -create ../%s/lib/libcsv.%s ../%s/lib/libcsv.%s -output libcsv.%s' % (self.install_x86_dir, libext, self.install_arm_dir, libext, libext))
+
+        self.copy('*.h', src='%s/include' % self.install_x86_dir, dst='include')
+        self.copy('libcsv.%s' % libext, src=self.install_universal_dir, dst='lib')
 
         self.copy('%s.txt' % self.name, src=self.source_dir, dst='license')
 
